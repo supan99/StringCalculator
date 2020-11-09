9390d49ebe8af184f633fca00475851fa50b5345
Supan Shah <55012690+supan99@users.noreply.github.com>
2020-11-10 01:29:28 +0530

    Module 5 & 6 : Calling Add with a negative number will throw an exception “negatives not allowed”

--- StringCalculator1.java
+++ StringCalculator1.java
@@ -1,24 +1,44 @@
 import java.util.Scanner;
 import java.util.*;
+class InvalidInputFormat extends Exception{
+	String msg;
+	InvalidInputFormat(String msg){
+		this.msg = msg;
+	}
+	public String toString(){
+		return this.msg;
+	}
+}
+class NegativeNumberException extends Exception{
+	int value;
+	NegativeNumberException(int value){
+		this.value=value;
+	}
+	public String toString(){
+		return "Number " + this.value +" is less than zero";
+	}
+}
 class StringCalculatorKada{
 	public static Scanner sc = new Scanner(System.in);
 	
 	public int Add(String input){
-		int sum;
-		if(input.equals(" ")|| input.equals("")){
+		int sum=-1;
+		if(input.equals(" ")){
 			sum = 0;
 		}else{
-			String numbers = extractNumbersFromInput(input);
-			if(numbers.equals("II")){
-			   sum = -1; 
-			}else{
+			try{
+				String numbers = extractNumbersFromInput(input);
 				sum = sumOfNumbers(numbers);
-			}
+			}catch(InvalidInputFormat msg){
+				System.out.println(msg);
+			}catch(NegativeNumberException msg){
+				System.out.println("NEGATIVES NOT ALLOWED");
+			}			
 		}
 		return sum;
 	}
 	
-	private static String extractNumbersFromInput(String input){
+	private static String extractNumbersFromInput(String input)throws InvalidInputFormat{
 		String delimiter = ",";
 		StringBuilder sb = new StringBuilder(input);
 		if((input.lastIndexOf("\\n")+2)!=input.length()){
@@ -27,19 +47,22 @@ class StringCalculatorKada{
 				sb.replace(position,position+2,delimiter);
 			}
 		}else{
-			sb.replace(0,input.length(),"II");
+			throw new InvalidInputFormat("INVALID INPUT");
 		}
 		return sb.toString();
 		
 	}
 	
-	private static int sumOfNumbers(String numbers){
+	private static int sumOfNumbers(String numbers)throws NegativeNumberException{
 		int sum=0;
 		StringTokenizer str = new StringTokenizer(numbers,",");
 		while(str.hasMoreTokens()){
 			int value = Integer.parseInt(str.nextToken());
+			if(value < 0){
+				throw new NegativeNumberException(value);
+			}
 			if(value <= 1000){
-				sum += value;
+				sum += value ;
 			}
 		}
 		return sum;
@@ -50,9 +73,7 @@ class StringCalculatorKada{
 		System.out.print("\nEnter String: ");
 		String input=sc.next();
 		int sum = strCalcKada.Add(input);
-		if(sum==-1){
-			System.out.println("\nINVALID INPUT"); 
-		}else{
+		if(sum >=0){	
 			System.out.println(sum);
 		}
 	}
